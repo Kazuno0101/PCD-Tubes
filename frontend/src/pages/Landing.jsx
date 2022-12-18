b5fea049e686053bf92e201deb45b5e16db3ec91
Aqil Rahman <aqilrahman23@gmail.com>
2022-12-19 01:58:46 +0700

    Landing Page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,201 @@
 const Landing = () => {
 	return (
 		<>
-			<h1>asdasd</h1>
+			<section class="px-2 py-32 bg-white md:px-0">
+				<div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+					<div class="flex flex-wrap items-center sm:-mx-3">
+						<div class="w-full md:w-1/2 md:px-3">
+							<div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+								<h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+									<span class="block xl:inline">Love</span>
+									<span class="block text-indigo-600 xl:inline">Colored.</span>
+								</h1>
+								<p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Ubah foto lama Anda menjadi warna alami, berikan tampilan baru secara gratis..</p>
+								<div class="relative flex flex-col sm:flex-row sm:space-x-4">
+									<a href="/colourfull" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+										Coba Gratis
+										<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="w-full md:w-1/2">
+							<div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+								<img src="https://cfcdn.apowersoft.info/projects/picwish/img/photo-colorization/banner-2.jpg" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="text-gray-700 body-font border-t border-gray-200">
+				<div class="container px-5 py-24 mx-auto">
+					<div class="flex flex-col text-center w-full mb-20">
+						<h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Cara mewarnai foto hitam putih di</h2>
+						<h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">LoveColored.</h1>
+					</div>
+					<div class="flex flex-wrap -m-4">
+						<div class="p-4 md:w-1/3">
+							<div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+								<div class="flex items-center mb-3">
+									<div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+										1
+									</div>
+									<h2 class="text-gray-900 text-lg title-font font-medium">Unggah Gambar</h2>
+								</div>
+								<div class="flex-grow">
+									<p class="leading-relaxed text-base">Pilih file foto dengan mengunggah atau menyeret.</p>
+								</div>
+							</div>
+						</div>
+						<div class="p-4 md:w-1/3">
+							<div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+								<div class="flex items-center mb-3">
+									<div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+										2
+									</div>
+									<h2 class="text-gray-900 text-lg title-font font-medium">Mewarnai</h2>
+								</div>
+								<div class="flex-grow">
+									<p class="leading-relaxed text-base">Tunggu beberapa saat dan foto Anda akan otomatis diwarnai..</p>
+								</div>
+							</div>
+						</div>
+						<div class="p-4 md:w-1/3">
+							<div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+								<div class="flex items-center mb-3">
+									<div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+										3
+									</div>
+									<h2 class="text-gray-900 text-lg title-font font-medium">Unduh</h2>
+								</div>
+								<div class="flex-grow">
+									<p class="leading-relaxed text-base">Simpan foto berwarna secara gratis.</p>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="text-gray-700 body-font border-t border-gray-200">
+				<div class="container px-5 py-24 mx-auto">
+					<div class="flex flex-col text-center w-full mb-20">
+						<h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">TIM</h1>
+						<p class="lg:w-2/3 mx-auto leading-relaxed text-base">LoveColored dikembangkan Tim Keceh si Paling SKS:</p>
+					</div>
+					<div class="flex flex-wrap -m-2">
+						<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+							<div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+								<img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://akademik.polban.ac.id/fotomhsrekap/201524036.jpg" />
+								<div class="flex-grow">
+									<h2 class="text-gray-900 title-font font-medium">Arsal Fadilah</h2>
+									<p class="text-gray-500">Backend Developer</p>
+								</div>
+							</div>
+						</div>
+						<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+							<div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+								<img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://akademik.polban.ac.id/fotomhsrekap/201524035.jpg" />
+								<div class="flex-grow">
+									<h2 class="text-gray-900 title-font font-medium">Aqil Rahman</h2>
+									<p class="text-gray-500">Frontend Developer</p>
+								</div>
+							</div>
+						</div>
+						<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+							<div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+								<img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://akademik.polban.ac.id/fotomhsrekap/201524057.jpg" />
+								<div class="flex-grow">
+									<h2 class="text-gray-900 title-font font-medium">Nuno Alwi Azimah</h2>
+									<p class="text-gray-500">Frontend Developer</p>
+								</div>
+							</div>
+						</div>
+						<div class="p-2 lg:w-1/2 md:w-1/2 w-full">
+							<div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+								<img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://akademik.polban.ac.id/fotomhsrekap/201524042.jpg" />
+								<div class="flex-grow">
+									<h2 class="text-gray-900 title-font font-medium">Fadhil Muhammad</h2>
+									<p class="text-gray-500">Software Tester</p>
+								</div>
+							</div>
+						</div>
+						<div class="p-2 lg:w-1/2 md:w-1/2 w-full">
+							<div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+								<img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://akademik.polban.ac.id/fotomhsrekap/201524063.jpg" />
+								<div class="flex-grow">
+									<h2 class="text-gray-900 title-font font-medium">Wili Akbar Nugraha</h2>
+									<p class="text-gray-500">Software Tester</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="text-gray-700 body-font border-t border-gray-200">
+				<div class="container px-5 py-24 mx-auto flex flex-wrap">
+					<div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+						<img alt="feature" class="object-cover object-center h-full w-full" src="https://cfcdn.apowersoft.info/projects/picwish/img/photo-colorization/3.jpg" />
+					</div>
+					<div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+						<div class="flex flex-col mb-10 lg:items-start items-center">
+							<div class="flex-grow">
+								<h2 class="text-gray-900 text-lg title-font font-medium mb-3">Pewarna foto gratis dan berguna</h2>
+								<p class="leading-relaxed text-base">Pewarna foto PicWish online benar-benar gratis untuk digunakan tanpa batasan, Anda dapat mengubah foto hitam putih Anda menjadi foto berwarna dalam hitungan detik tanpa keahlian apa pun..</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<footer class="text-gray-700 body-font">
+				<div class="border-t border-gray-200">
+					<div class="container px-5 py-8 flex flex-wrap mx-auto items-center">
+						<div class="flex md:flex-no-wrap flex-wrap justify-center md:justify-start">
+							<input class="sm:w-64 w-40 bg-gray-100 rounded sm:mr-4 mr-2 border border-gray-400 focus:outline-none focus:border-indigo-500 text-base py-2 px-4" placeholder="Email" type="text" />
+							<button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Subscribe</button>
+							<p class="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Dapatkan informasi terupdate
+								<br class="lg:block hidden" />melalui email!
+							</p>
+						</div>
+						<span class="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
+							<a class="text-gray-500">
+								<svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+									<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+								</svg>
+							</a>
+							<a class="ml-3 text-gray-500">
+								<svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+									<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+								</svg>
+							</a>
+							<a class="ml-3 text-gray-500">
+								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+									<rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+									<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+								</svg>
+							</a>
+							<a class="ml-3 text-gray-500">
+								<svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+									<path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+									<circle cx="4" cy="4" r="2" stroke="none"></circle>
+								</svg>
+							</a>
+						</span>
+					</div>
+				</div>
+				<div class="bg-gray-200">
+					<div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+						<p class="text-gray-500 text-sm text-center sm:text-left">© 2020 LoveColored. —
+							<a href="#!" class="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@LoveColored.</a>
+						</p>
+						<span class="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Arsal, Aqil, Nuno, Fadhil, Wili</span>
+					</div>
+				</div>
+			</footer>
 		</>
 	);
 };
